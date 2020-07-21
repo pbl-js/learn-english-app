f6504135388958fb5875c0f7e04062989dc0eb8b
Paweł Miłczak <jogobello4@gmail.com>
2020-07-22 00:21:28 +0200

    Install dependencies and add basic graphql structure

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const { graphqlHTTP } = require("express-graphql");
+const graphQlSchema = require("./graphql/schema/index");
+const graphQlResolvers = require("./graphql/resolvers/index");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true,
+  })
+);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.tm6sw.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(
+    app.listen(3000, () =>
+      console.log(`app listen on port 3000. Database is connected`)
+    )
+  )
+  .catch((err) => console.log(err));
